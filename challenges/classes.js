b1f1b7fa543958e167658678f31e4d9326f2bf8b
andy-caruso <andycaruso@zohomail.com>
2020-06-19 14:24:41 -0500

    updated the stretch goal based on Brit's suggestion

--- challenges/classes.js
+++ challenges/classes.js
@@ -17,12 +17,32 @@ class CuboidMaker{
     }
   surfaceArea(){
     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }  
+}
+
+  class CubeMaker extends CuboidMaker{
+      constructor(attributes){
+          super(attributes)
+      }
+      cubeVolume(){
+          return (this.length * this.width * this.height)
+      }
+      cubeSurface(){
+          return ((this.length * this.height) * 4) + ((this.width * this.height) * 2)
+      }
   }
+
   const cuboid = new CuboidMaker({
     length: 4,
     width: 5,
     height: 5,
   });
-}
+  const newCuboid = new CubeMaker({
+    length: 5,
+    width: 5,
+    height: 5,
+  });
   console.log(cuboid.volume());
   console.log(cuboid.surfaceArea());
+  console.log(newCuboid.cubeVolume());
+  console.log(newCuboid.surfaceArea());
